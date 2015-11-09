520f78784754dc267b32a08cdfb6901580a33f47
Niklas Hagner <niklas.hagner@outlook.com>
2015-11-09 22:57:00 +0100

    Refactored height/width vars

--- imgCropper.js
+++ imgCropper.js
@@ -1,22 +1,23 @@
-$(document).ready(function() {
-	changeImages();
+$(document).ready(function () {
+    changeImages();
 });
 
 function changeImages_pureJS() {
-	for(i=0; i<document.images.length; i++) {
-		var img = document.images[i];
-		var h = img.height, w = img.width;
-		var marginTop ="-"+(h/2)+"px",  marginLeft = "-"+(w/2)+"px";
-		img.style.marginLeft = marginLeft;
-	  img.style.marginTop = marginTop;
-	}
+    for (i = 0; i < document.images.length; i++) {
+        var img = document.images[i];
+        var marginTop = "-" + (img.height / 2) + "px",
+            marginLeft = "-" + (img.width / 2) + "px";
+        img.style.marginLeft = marginLeft;
+        img.style.marginTop = marginTop;
+    }
 
 }
+
 function changeImages() {
-	$('.cropped img').each(function() {
-    var h = $(this).height, w = $(this).width;
-    var marginTop ="-"+(h/2)+"px",  marginLeft = "-"+(w/2)+"px";
-    this.style.marginLeft = marginLeft;
-    this.style.marginTop = marginTop;
-	});
+    $('.cropped img').each(function (img) {
+        var marginTop = "-" + (img.height / 2) + "px",
+            marginLeft = "-" + (img.width / 2) + "px";
+        this.style.marginLeft = marginLeft;
+        this.style.marginTop = marginTop;
+    });
 }
